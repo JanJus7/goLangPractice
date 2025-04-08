ea3c300242190de084cac07f3ddb57d6c14588ec
Jan <janjus2004@gmail.com>
2025-04-08 23:28:16 +0200

    added methods for directory

--- lab3/interfejs.go
+++ lab3/interfejs.go
@@ -112,6 +112,52 @@ func NewDirectory(name string, parentPath string) *DirectoryImpl {
 	}
 }
 
+func (f *DirectoryImpl) Name() string {
+	return f.name
+}
+
+func (f *DirectoryImpl) Path() string {
+	return f.parentPath + "/" + f.name
+}
+
+func (f *DirectoryImpl) Size() int64 {
+	return int64(len(f.items))
+}
+
+func (f *DirectoryImpl) CreatedAt() time.Time {
+	return f.created
+}
+
+func (f *DirectoryImpl) ModifiedAt() time.Time {
+	return f.modified
+}
+
+func (f *DirectoryImpl) AddItem(item FileSystemItem) error {
+	for _, existingItem := range f.items {
+		if existingItem.Name() == item.Name() {
+			return ErrItemExists
+		}
+	}
+	f.items = append(f.items, item)
+	f.modified = time.Now()
+	return nil
+}
+
+func (f *DirectoryImpl) RemoveItem(name string) error {
+	for i, item := range f.items {
+		if item.Name() == name {
+			f.items = append(f.items[:i], f.items[i+1:]...)
+			f.modified = time.Now()
+			return nil
+		}
+	}
+	return ErrItemNotFound
+}
+
+func (f *DirectoryImpl) Items() []FileSystemItem {
+	return f.items
+}
+
 type SymLink struct {
 	name       string
 	parentPath string
